ca0521c0569150122931a51eca823ce577064dd1
Dao Tuan <heryhudson@gmail.com>
2021-02-24 11:16:25 +0700

    asd

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 console.log("works");
+console.log(process.env.GOOGLE_KEY_FILE_NAME);
+console.log(JSON.parse(process.env.GOOGLE_KEY_FILE_NAME));
+console.log(JSON.parse(process.env.GOOGLE_KEY_FILE_NAME).repository);
